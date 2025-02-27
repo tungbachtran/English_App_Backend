7d0ecbed8468b1aa485209e670003090feb6514c
{Tran Bach Tung} <tung2982004@gmail.com>
2025-02-27 21:43:54 +0700

    change

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const app = express();
 require("dotenv").config();
 var path = require('path');
-
+const cors = require('cors');
+const corsConfig = {
+  origin: "*",
+  credential: true,
+  methods: ["GET", "POST","PUT","DELETE"],
+};
+app.options("",cors(corsConfig));
+app.use(cors(corsConfig));
 const database = require('./config/database');
 const route = require("./routes/index.route");
-const app = express();
+
 const port = process.env.PORT ;
 app.use(express.static(`${__dirname}/public`));
 database.connect();
